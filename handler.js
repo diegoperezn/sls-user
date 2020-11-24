249ee0d29f42fbc449e68a66fce3111513be75c8
Diego Perez <diego@Diegos-MacBook-Pro.local>
2020-11-24 17:09:45 -0300

    add get and post

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.list = async event => {
     return {
         statusCode: 200,
         body: JSON.stringify({
@@ -10,7 +10,17 @@ module.exports.hello = async event => {
             2
         ),
     };
+};
 
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+module.exports.create = async event => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+                message: 'Go Serverless v1.0! Your function executed successfully !',
+            },
+            null,
+            2
+        ),
+    };
 };
